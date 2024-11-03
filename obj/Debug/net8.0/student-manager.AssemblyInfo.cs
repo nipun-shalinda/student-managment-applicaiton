4291037e28b021423ae6ddde84621ea2a6694ee7
nipun-shalinda <nipunshalinda10@gmail.com>
2024-11-03 08:34:43 +0530

    created

--- obj/Debug/net8.0/student-manager.AssemblyInfo.cs
+++ obj/Debug/net8.0/student-manager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("student-manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79b07e9844d2b9798bfe46616398811481d26b40")]
 [assembly: System.Reflection.AssemblyProductAttribute("student-manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("student-manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
